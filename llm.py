2f5ff46ca896f4373f2279666ce425cbd5c56235
Luis Correa <l.correaz@uniandes.edu.co>
2024-09-24 14:54:16 +0000

    update information

--- llm.py
+++ llm.py
@@ -1,6 +1,15 @@
 import streamlit as st
+from langchain_openai import OpenAIEmbeddings
 
 # Create the LLM
+from langchain_openai import ChatOpenAI
 
+llm = ChatOpenAI(
+    openai_api_key=st.secrets["OPENAI_API_KEY"],
+    model=st.secrets["OPENAI_MODEL"],
+)
 
 # Create the Embedding model
+embeddings = OpenAIEmbeddings(
+    openai_api_key=st.secrets["OPENAI_API_KEY"]
+)
